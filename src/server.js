f095831dd63271ab4e6c4ea9b53dab0784ec5fd0
Wallace Júnior <wflj1997@gmail.com>
2024-01-01 13:48:35 -0300

    Corpo da requisição em JSON (Streams & Buffers)

--- src/server.js
+++ src/server.js
@@ -2,9 +2,23 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
   response.setHeader("Content-Type", "application/json; charset=utf-8");
+
+  if (request.headers["content-type"]?.startsWith("application/json")) {
+    const buffers = [];
+
+    for await (const chunk of request) {
+      buffers.push(chunk);
+    }
+
+    try {
+      request.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch (error) {
+      request.body = null;
+    }
+  }
 
   if (method === "GET" && url === "/users") {
     return response.writeHead(200).end(JSON.stringify(users));
@@ -13,8 +27,8 @@ const server = http.createServer((request, response) => {
   if (method === "POST" && url === "/users") {
     const newUser = {
       id: crypto.randomUUID(),
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name: request.body.name,
+      email: request.body.email,
     };
 
     users.push(newUser);
