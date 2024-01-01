4201ffe50af7e096e146527adf0b55e3199ff1a0
Wallace Júnior <wflj1997@gmail.com>
2024-01-01 12:24:03 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
+  response.setHeader("Content-Type", "application/json; charset=utf-8");
 
   if (method === "GET" && url === "/users") {
-    return response.end("Listagem de usuários");
+    return response.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return response.end("Criação de usuário");
+    const newUser = {
+      id: crypto.randomUUID(),
+      name: "John Doe",
+      email: "johndoe@example.com",
+    };
+
+    users.push(newUser);
+    response.statusCode = 201;
+    return response.end(JSON.stringify(newUser));
   }
 
   response.statusCode = 405;
-  return response.end("Method not allowed");
+  return response.end(JSON.stringify({ error: "Method not allowed" }));
 });
 
 const port = 3333;
