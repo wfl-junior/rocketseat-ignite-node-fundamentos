70274d1ec34957e10a89b07f9c534115bbd2d038
Wallace Júnior <wflj1997@gmail.com>
2024-01-01 15:52:44 -0300

    Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,15 +1,30 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then(database => (this.#database = JSON.parse(database)))
+      .catch(() => this.#persist());
+  }
+
+  async #persist() {
+    await fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2));
+  }
 
   select(table) {
     return this.#database[table] ?? [];
   }
 
-  insert(table, data) {
+  async insert(table, data) {
     if (!Array.isArray(this.#database[table])) {
       this.#database[table] = [];
     }
 
     this.#database[table].push(data);
+    await this.#persist();
   }
 }
