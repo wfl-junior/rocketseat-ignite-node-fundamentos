4f8d4fbbb0bc9252b0d178bc069c26e00351a5b5
Wallace Júnior <wflj1997@gmail.com>
2024-01-01 16:24:21 -0300

    Route e Query parameters

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
 
+const port = 3333;
+
 const server = http.createServer(async (request, response) => {
-  const { method, url } = request;
   await json(request, response);
+  const url = new URL(request.url, `http://localhost:${port}`);
+  request.query = Object.fromEntries(url.searchParams);
 
   for (const route of routes) {
-    if (route.method === method && route.path === url) {
+    if (route.method === request.method && route.path === url.pathname) {
       return route.handler(request, response);
     }
   }
@@ -15,6 +18,5 @@ const server = http.createServer(async (request, response) => {
   return response.writeHead(404).end();
 });
 
-const port = 3333;
 console.log(`Listening at http://localhost:${port}`);
 server.listen(port);
